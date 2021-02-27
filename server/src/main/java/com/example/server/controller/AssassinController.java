18958d78e37ae462692ac5d66934c7127435fe1c
Devin Almonor <74159713+DevKWA@users.noreply.github.com>
2021-02-27 00:07:44 -0500

    Update AssassinController.java

--- server/src/main/java/com/example/server/controller/AssassinController.java
+++ server/src/main/java/com/example/server/controller/AssassinController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/assassin")
-// http://localhost:8080/mexican
+
 public class AssassinController {
     @Autowired
     AssassinService assassinService;
